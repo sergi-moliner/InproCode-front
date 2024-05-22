512d3730b24d106e07ac7d47c290bd436127f615
sergi.moliner <sergilopezmoliner@gmail.com>
2024-05-22 12:52:42 +0200

    basic components, navbar and routing

---
+++ src/app/components/graphics/graphics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphics',
+  standalone: true,
+  imports: [],
+  templateUrl: './graphics.component.html',
+  styleUrl: './graphics.component.scss'
+})
+export class GraphicsComponent {
+
+}
