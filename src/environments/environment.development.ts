df7a6f855a7a28648fec43e91d915f90207c861a
sergi.moliner <sergilopezmoliner@gmail.com>
2024-06-11 12:17:22 +0200

    env update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
   endpoint: 'http://localhost:3000/',
-  mapboxToken: 'pk.eyJ1Ijoic2VyMTIyIiwiYSI6ImNseDBvZGV3ZTAybHAyanM5eXg3dGpsdXQifQ.o8iVw03nDw99AymVVgjRnw'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   endpoint: 'http://localhost:3000/',
-  mapboxToken: 'pk.eyJ1Ijoic2VyMTIyIiwiYSI6ImNseDBvZGV3ZTAybHAyanM5eXg3dGpsdXQifQ.o8iVw03nDw99AymVVgjRnw'
+mapboxToken: 'YOUR_MAPBOX_TOKEN'
 };
